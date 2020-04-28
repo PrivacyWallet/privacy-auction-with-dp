01e85e3404b5b347a9c134604f92058395e53fbe
FKYnJYQ <loveress01@gmail.com>
2020-04-28 14:02:07 +0800

    feat(all): finish contract

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,22 +1,84 @@
 pragma solidity >0.5.0;
 import "./DataBuyerInterface.sol";
 
-contract Calculator {
-  uint constant factor = 10000000;
-  address calculator;
+struct DataOwner {
+  uint price;
+  int encrypted_data;
+  uint epsilon;
+}
 
-  struct DataOwner {
-    uint price;
-    int encrypted_data;
-    uint epsilon;
-  }
+struct IndexValue { uint keyIndex; DataOwner value; }
+struct KeyFlag { address key; bool deleted; }
+
+struct itmap {
+    mapping(address => IndexValue) data;
+    KeyFlag[] keys;
+    uint size;
+}
+
+library IterableMapping {
+    function insert(itmap storage self, address key, DataOwner memory value) internal returns (bool replaced) {
+        uint keyIndex = self.data[key].keyIndex;
+        self.data[key].value = value;
+        if (keyIndex > 0)
+            return true;
+        else {
+            self.keys.push();
+            keyIndex = self.keys.length;
+            self.data[key].keyIndex = keyIndex + 1;
+            self.keys[keyIndex].key = key;
+            self.size++;
+            return false;
+        }
+    }
+
+    function remove(itmap storage self, address key) internal returns (bool success) {
+        uint keyIndex = self.data[key].keyIndex;
+        if (keyIndex == 0)
+            return false;
+        delete self.data[key];
+        self.keys[keyIndex - 1].deleted = true;
+        self.size --;
+    }
+
+    function contains(itmap storage self, address key) internal view returns (bool) {
+        return self.data[key].keyIndex > 0;
+    }
+
+    function iterate_start(itmap storage self) internal view returns (uint keyIndex) {
+        return iterate_next(self, uint(-1));
+    }
+
+    function iterate_valid(itmap storage self, uint keyIndex) internal view returns (bool) {
+        return keyIndex < self.keys.length;
+    }
+
+    function iterate_next(itmap storage self, uint keyIndex) internal view returns (uint r_keyIndex) {
+        keyIndex++;
+        while (keyIndex < self.keys.length && self.keys[keyIndex].deleted)
+            keyIndex++;
+        return keyIndex;
+    }
+
+    function iterate_get(itmap storage self, uint keyIndex) internal view returns (address key, DataOwner storage value) {
+        key = self.keys[keyIndex].key;
+        value = self.data[key].value;
+    }
+}
+contract Calculator {
+  uint constant upper_bound = 10000000;
+  address calculator;
 
   struct DataBuyer {
     DataBuyerInterface buyer_contract;
+    address[] selected_owner;
+    uint[] selected_prices;
+    uint budget;
   }
 
-  mapping(address => DataOwner) public data;
-  mapping(address => DataBuyer) public buyers;
+  itmap public data;
+  using IterableMapping for itmap;
+  mapping(address => DataBuyer) public transactions;
 
 
   // step 1
@@ -29,14 +91,18 @@ contract Calculator {
   // send encrypted data and epsilon.
   function set_data(uint price, int encrypted_data, uint epsilon, address payable _address) public {
     // TODO: assert
-    data[_address].encrypted_data = encrypted_data; 
-    data[_address].epsilon = epsilon;
-    data[_address].price = price;
+
+    DataOwner memory data_owner = DataOwner({
+      encrypted_data: encrypted_data,
+      epsilon: epsilon,
+      price: price
+    });
+    data.insert(_address, data_owner);
   }
 
   // step 7
   // event to notify off-chain calculator.
-  event data_selected(int[] owners_data, int[] owners_epsilon);
+  event data_selected(int[] owners_data, uint[] owners_epsilon);
 
   // step 4
   // data buyer provide it's budget by `payable`.
@@ -44,40 +110,67 @@ contract Calculator {
   function bid(DataBuyerInterface data_buyer_contract) public payable {
 
     // store contract address
-    buyers[msg.sender].buyer_contract = data_buyer_contract;
+    transactions[msg.sender].buyer_contract = data_buyer_contract;
     
     // step 5, 6
     // calculator call data_buyer's contract 
     // to provide epsilon's and budget number,
     // and waiting for result.
-    // TODO: we mock it currently
-    int[] memory temp = new int[](100);
-    for(uint i = 0; i < temp.length; i++){
-      temp[i] = int(i);
+    uint[] memory price_vec = new uint[](data.size);
+    uint[] memory epsilon_vec = new uint[](data.size);
+    int[] memory data_vec = new int[](data.size);
+    address[] memory address_vec = new address[](data.size);
+    for(uint i = data.iterate_start();
+       data.iterate_valid(i);
+       (i = data.iterate_next(i), i++)) {
+         (address _address,DataOwner storage data_owner) = data.iterate_get(i);
+         price_vec[i] = data_owner.price;
+         epsilon_vec[i] = data_owner.epsilon;
+         data_vec[i] = data_owner.encrypted_data;
+         address_vec[i] = _address;
     }
 
-    DataBuyerInterface(data_buyer_contract).send_budget_and_epsilons(1, temp);
+    uint[] memory results = DataBuyerInterface(data_buyer_contract).send_budget_and_epsilons(msg.value, epsilon_vec, price_vec);
 
-    int[] memory a = new int[](100);
-
-    for(uint i = 0; i < a.length; i++){
-      a[i] = int(i);
+    // select data we want.
+    address[] memory result_addresses = new address[](results.length);
+    int[] memory result_data = new int[](results.length);
+    uint[] memory result_epsilons = new uint[](results.length);
+    uint[] memory result_prices = new uint[](results.length);
+    for(uint i = 0; i < results.length; i++ ){
+      result_addresses[i] = address_vec[results[i]];
+      result_data[i] = data_vec[results[i]];
+      result_epsilons[i] = epsilon_vec[results[i]];
+      result_prices[i] = price_vec[results[i]];
     }
-    
+
+    // suspend this transactions
+    transactions[msg.sender].selected_owner = result_addresses;
+    transactions[msg.sender].selected_prices = result_prices;
+    transactions[msg.sender].budget = msg.value;
+
     // step 7
     // trigger the event, tell the calculator
     // that he may continue the computation.
     
-    emit data_selected(a,a);
+    emit data_selected(result_data,price_vec);
     
   }
 
   // step 9, 10
+  // send money to buyer & owner.
   function bidEnd(address data_buyer,int encrypted_result) public {
 
-    
+    uint sum = 0;
+    for(uint i = 0; i < transactions[data_buyer].selected_prices.length; i++){
+      transactions[data_buyer].selected_owner[i].call{value:transactions[data_buyer].selected_prices[i] }("");
+      sum += transactions[data_buyer].selected_prices[i];
+    }
+    transactions[data_buyer].buyer_contract.send_result(encrypted_result);
+    data_buyer.call{value: transactions[data_buyer].budget-sum}("");
 
   }
+
    //say hello world
   function say() public pure returns (string memory) {
     return "Hello World";
--- contracts/DataBuyer.sol
+++ contracts/DataBuyer.sol
@@ -4,6 +4,8 @@ import "./DataBuyerInterface.sol";
 contract DataBuyer is DataBuyerInterface {
     address dataBuyer;
     int result;
+    uint constant upper_bound = 10000000;
+    uint[]  result_index;
 
     // Constructor code is only run when the contract
     // is created
@@ -12,16 +14,95 @@ contract DataBuyer is DataBuyerInterface {
       dataBuyer = msg.sender;
     }
 
-  function send_budget_and_epsilons(int budget, int[] calldata epsilons) external view returns (uint[] memory){
-      // now we have epsilons and budget,
-      // we select from them and return the index
-      
-      
+    function qsort(uint[] memory data) private pure {
+       quickSort(data, int(0), int(data.length - 1));
+    }
+    
+    function quickSort(uint[] memory arr, int left, int right) private pure{
+        int i = left;
+        int j = right;
+        if(i==j) return;
+        uint pivot = arr[uint(left + (right - left) / 2)];
+        while (i <= j) {
+            while (arr[uint(i)] > pivot) i++;
+            while (pivot > arr[uint(j)]) j--;
+            if (i <= j) {
+                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
     }
 
-    function send_result(int _result) external{
-      result = _result;
-    }
+   function sqrt(uint x) internal pure returns (uint){
+       uint n = x / 2;
+       uint lstX = 0;
+       while (n != lstX){
+           lstX = n;
+           n = (n + x/n) / 2; 
+       }
+       return uint(n);
+   }
+
+   function calculate_theta_vec(uint budget, uint[] memory epsilons)private view returns (uint[] memory) {
+ 
+      qsort(epsilons);
+
+      uint[] memory sum_of_square_vec = new uint[](epsilons.length);
+      for(uint i = epsilons.length-2; i >=0 ; i--) {
+        sum_of_square_vec[i] = ((sum_of_square_vec[i+1]*
+            epsilons[i+1]**2) + epsilons[i]**2) / epsilons[i]**2;
+      }
+
+      uint[] memory theta_vec = new uint[](epsilons.length);
+      
+      for(uint i = 0; i< theta_vec.length; i++){
+        if(theta_vec.length - i < budget) {
+          // alternative 1: sum of square is constraints
+          for(uint j = i; j< theta_vec.length; j++) {
+            theta_vec[j] = upper_bound;
+          }
+          break;
+        } else if (sum_of_square_vec[i] > budget) {
+          // alternative 2:
+          uint factor = sqrt(budget / (sum_of_square_vec[i]*(epsilons[i]**2)));
+
+          for(uint j = i; j< theta_vec.length; j++){
+            theta_vec[j] = factor * epsilons[j];
+          }
+          break;
+        } else {
+          // alternative 3: 0 < \theta < 1 is constraints
+          budget -= 1;
+          theta_vec[i] = 1;
+        }
+      }
+      return theta_vec;
+
+  }
+
+
+  // step 5, 6
+  // now we have epsilons and budget,
+  // we select from them and return the index
+  function send_budget_and_epsilons(uint budget, uint[] calldata epsilons, uint[] calldata prices) override external returns (uint[] memory){
+
+      uint[] memory theta_vec = calculate_theta_vec(budget, epsilons);
+      for(uint i = 0; i< epsilons.length; i++) {
+        if ( theta_vec[i] >= prices[i] ) {
+          result_index.push(i);
+        }
+      }
+      return result_index;
+   }
+
+  function send_result(int _result) override external{
+    result = _result;
+  }
 }
 
 
