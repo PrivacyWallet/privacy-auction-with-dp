10a02cdbe0ce7e08a079780b87cc16acf327d7bf
UgOrange <v1mer@protonmail.com>
2020-07-19 12:27:46 +0800

    add js

--- JS/api.js
+++ JS/api.js
@@ -2,12 +2,33 @@ const abi = require('./abi.js')
 var NodeRSA = require('node-rsa')
 var fs = require('fs')
 var Tx = require('ethereumjs-tx');
+async function deployBuyer()
+{
+  
+}
 async function createNewTransaction(   filter,
     selectType,
     queryType,
     budget,
-    calculatorAddress,buyerAddress)
+    calculatorAddress,bindata)
     {
+      myContract.deploy({
+        data: bindata,
+    })
+    .send({
+        from: window.web3.eth.coinbase,
+        gas: 1500000,
+        gasPrice: '30000000000000'
+    }, function(error, transactionHash){ ... })
+    .on('error', function(error){ ... })
+    .on('transactionHash', function(transactionHash){ ... })
+    .on('receipt', function(receipt){
+       console.log(receipt.contractAddress) // 收据中包含了新的合约地址
+    })
+    .on('confirmation', function(confirmationNumber, receipt){ ... })
+    .then(function(newContractInstance){
+        console.log(newContractInstance.options.address) // 新地址的合约实例
+    });
         contract=new web3.eth.Contract(abi.Calc, calculatorAddress)
         await contract.methods.bid(calculatorAddress,"median").send(
             {
