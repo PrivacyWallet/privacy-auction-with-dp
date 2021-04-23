21ca4c72b8d2ae95f90c59a64e56b8723fb04603
FKYnJYQ <loveress01@gmail.com>
2021-04-23 15:42:06 +0800

    remove multiple deploy

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,7 +2,6 @@
 const IterableMapping = artifacts.require("IterableMapping")
 const DataBuyer = artifacts.require("DataBuyer");
 const Calculator = artifacts.require("Calculator");
-const Calculator2 = artifacts.require("Calculator");
 
 module.exports = function(deployer) {
   //deployer.deploy(DataBuyerInterface)
@@ -11,6 +10,4 @@ module.exports = function(deployer) {
   deployer.deploy(IterableMapping)
   deployer.link(IterableMapping, Calculator)
   deployer.deploy(Calculator);
-  deployer.link(IterableMapping, Calculator2)
-  deployer.deploy(Calculator2);
 }
